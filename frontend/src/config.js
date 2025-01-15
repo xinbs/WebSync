91b94223603171588e22b5d18de303a28e2d9c21
xinbs <xinquanliang@qq.com>
2025-01-15 14:56:06 +0800

    refactor: 优化 API 地址配置，移除硬编码的 IP 地址

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,10 +1,11 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 const config = {
-  // 如果是开发环境使用 localhost，否则使用当前域名
+  // 如果是开发环境使用 localhost
+  // 生产环境使用相同域名，只是端口不同
   apiBaseUrl: isDevelopment 
     ? 'http://localhost:5002'
-    : `http://${window.location.hostname}:5002`
+    : `${window.location.protocol}//${window.location.hostname}:5002`
 };
 
 export default config; 
